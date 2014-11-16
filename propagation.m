41789d96e5174065e3eb860118ed513b7a76b82b
Smita Patankar <smita.patankar.313@gmail.com>
2014-11-15 20:16:29 -0500

    Add running script but searchCyclesAndSetValues doesn't work yet

--- propagation.m
+++ propagation.m
@@ -1,13 +1,23 @@
-function [B, b] = propagation(bk, ak, a, c, omega)
-b = zeros(1, length(c));
-extShock = 0;
+function [B, b] = propagation(bk, ak, aik, a, c, omega, graph)
+extShock = 0; % calculate external shock
+
+for i = 1:length(c)
+    for j = 1:length(c)
+        if (i==j)
+            c(i,j) = 0;
+        end
+    end
+end
 
 for i = 1:length(ak)
-    extShock = extShock + bk(1, i) * ak(1, i);
+    extShock = extShock + bk(1,i) * ak(1,i);
 end
+
+t = 1;
+b = zeros(1, length(c));
 condition = true;
 while(condition)
-    lambda = bk * ak + b * c; % compute lambda
+    lambda = bk * aik + b * c; % compute lambda
     B = [];
     b = [];  
     v = [];
@@ -15,11 +25,9 @@ while(condition)
     for i = 1:length(lambda) % compute B and b
         r = 0;
         d = 0;
-        for j = 1:length(c)
-            if(i ~= j)
-                r = r + c(j,i);
-                d = d + c(i,j);
-            end
+        for j = 1:length(c)            
+            r = r + c(j,i);
+            d = d + c(i,j);
         end
         vi = a(1,i) + r + d;
         v = [v, vi];
@@ -30,15 +38,20 @@ while(condition)
     end
     
     if(length(omega) >= 2)
-        % search for cycles
-        % set bi
+        b = searchCyclesAndSetValues(b, graph, c);
     end
     
     Bv = 0;
     for i = 1:length(omega)
         Bv = Bv + B(1, i) * v(1, i);
     end
-    if(Bv >= extShock)
+    if( t > 2)%Bv >= extShock)
         condition = false;
     end
+    fprintf('Iteration %d:\n', t);
+    fprintf('[B]:' );
+    disp(B);
+    fprintf('[b]:' );
+    disp(b);
+    t = t + 1;
 end
---
+++ searchCyclesAndSetValues.m
@@ -0,0 +1,4 @@
+function b = searchCyclesAndSetValues(b, graph, c)
+% search for cycles in omega and set bi = 1 if
+% cycle has no debts with outside nodes
+end
